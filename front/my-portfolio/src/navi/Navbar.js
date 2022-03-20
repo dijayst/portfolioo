60407370b600cdd26e2d8c1d6758c5252ad13e6a
dijayst <dijayst@gmail.com>
2022-03-20 01:21:34 +0100

    responsive

--- front/my-portfolio/src/navi/Navbar.js
+++ front/my-portfolio/src/navi/Navbar.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Link } from 'react-router-dom'
 import Home from './Home'
 import nab from './nab.jpg';
@@ -6,13 +6,13 @@ import nab from './nab.jpg';
 
 const Navbar = () => {
    
-  //const [click, setClick] = useState(false);
+const [click, setClick] = useState(false);
 
- // const handleClick = () => setClick(!click);
+  const handleClick = () => setClick(!click);
 
   // const closeMobileMenu = () => setClick(false);
 
-
+console.log({click})
     return (
         <div>
             
@@ -32,11 +32,11 @@ const Navbar = () => {
             </Link>
            
                   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                  <span className=""> helo</span>
+                  <span className={click ?'fas fa-times' : 'fas fa-bars'} onClick={handleClick}> helo</span>
                   </button>
                   <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     
-                     <ul className="navbar-nav">
+                     <ul className={click ? 'nav-menu active' : 'nav-menu'}>
                         <li className="nav-item active">
                         <Link  to="/"
               className='nav-links'
