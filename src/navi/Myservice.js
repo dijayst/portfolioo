8e1b4f28cb344d89f2c7aecf38cc4d341e92a4e7
dijayst <dijayst@gmail.com>
2022-06-04 12:59:55 +0100

    hr

--- src/navi/Myservice.js
+++ src/navi/Myservice.js
@@ -42,7 +42,7 @@ textAlign:"center"
    </p>
 <div className="tile-row">
 {
- whatido.map((item)=>{
+ whatido.map((item,index)=>{
 return(
    <div key={item.id} className="tile">
  <img src={item.market} alt="hrk" height="70px" width="70px" className="marketimage"/>
