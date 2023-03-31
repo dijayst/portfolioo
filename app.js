c46d49ac67e5ed6b120189e6a4c191692fecc87f
dijayst <dijayst@gmail.com>
2023-03-31 01:44:56 +0100

    trh

--- app.js
+++ app.js
@@ -1,14 +1,7 @@
 const express =require("express");
-const multer=require('multer')
-const path=require('path')
 const app= express();
-const bodyParser=require('body-parser')
-const cors=require("cors");
 const PORT=process.env.PORT|| 5050;
-const connection=mongoose.connection;
-connection.once("open",()=>{
-    console.log("it connected successfully");
-})
+app.get('/',(req,res)=>res.send("hellop"))
 app.listen(process.env.PORT || PORT,()=>{
     console.log("server running on port 5050")
 })
