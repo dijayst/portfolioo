3fa3a3cb90ef697576bd38f4a4bda0920de4e33e
dijayst <dijayst@gmail.com>
2023-04-06 02:34:32 +0100

    tyfyt

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ const PORT=process.env.PORT|| 5050;
 
 //const PORT=process.env.MONGODB_CONNECTION_STRING|| 5050;
 //const PORT=5050;
-const bcrypt =require('bcrypt')
+const bcrypt =require('bcryptjs')
 const session =require("express-session");
 const cookieParser = require("cookie-parser")
 const saltRounds =10
